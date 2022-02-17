a96c35aab903a10d61f81ad4be299b51751ebcd0
T Gibson <taylornicolegibson@gmail.com>
2022-02-17 17:12:59 -0500

    progress

--- UI/Level.gd
+++ UI/Level.gd
@@ -5,6 +5,9 @@ func show_labels():
 	get_tree().paused = true
 	$Title.text = Global.levels[Global.level]["title"]
 	$Subtitle.text = Global.levels[Global.level]["subtitle"]
+	var other_level = get_node_or_null("/root/Game/UI/HUD/Level")
+	if other_level != null:
+		other_level.hide()
 	show()
 	$Timer.start()
 
@@ -12,3 +15,8 @@ func show_labels():
 func _on_Timer_timeout():
 	hide()
 	get_tree().paused = false
+
+	var other_level = get_node_or_null("/root/Game/UI/HUD/Level")
+	if other_level != null:
+		other_level.text = Global.levels[Global.level]["title"]
+		other_level.show()
